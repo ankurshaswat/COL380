4a4aff5fcbc94f5545b670a0f0c9b2bf60abbd57
Shashwat Shivam <shivamshaswat@gmail.com>
2019-04-04 00:02:06 +0530

    Added cuda code base

--- lab3/code/lab3_cuda.cu
+++ lab3/code/lab3_cuda.cu
@@ -1,124 +1,140 @@
 #include "lab3_cuda.h"
 
-inline int INDEX(int i1, int i2, int l1, int l2) { return i1 * l2 + i2; }
+__device__ inline int INDEX(int i1, int i2, int l1, int l2) {
+  return i1 * l2 + i2;
+}
 
-int state, *ind, N;
-bool *changed;
-double *E, *e, *S;
-
-int MAXIND(int k) {
+__device__ void MAXIND(int k, int N, double *S, int *result) {
   int m = k + 1, i;
   for (i = k + 2; i < N; i++) {
-    if (fabs(S[INDEX(k, i, N, N)]) > fabs(S[INDEX(k, m, N, N)])) {
+    if (fabsf(S[INDEX(k, i, N, N)]) > fabsf(S[INDEX(k, m, N, N)])) {
       m = i;
     }
   }
-  return m;
+  *result = m;
 }
 
-__global__ void UPDATE(int k, double t) {
+__global__ void UPDATE(int k, double t, double *e, bool *changed, int *state) {
   double y = e[k];
   e[k] = y + t;
   if (changed[k] && y == e[k]) {
     changed[k] = false;
-    state--;
+    (*state)--;
   } else if (!changed[k] && !(y == e[k])) {
     changed[k] = true;
-    state++;
+    (*state)++;
   }
 }
 
-void ROTATE(int k, int l, int i, int j, double c, double s) {
+__device__ void ROTATE(int k, int l, int i, int j, double c, double s,
+                       double *S, int N) {
   double Skl = S[INDEX(k, l, N, N)], Sij = S[INDEX(i, j, N, N)];
   S[INDEX(k, l, N, N)] = c * Skl - s * Sij;
   S[INDEX(i, j, N, N)] = s * Skl + c * Sij;
 }
 
-void JACOBI() {
-  int i, j, k, m, l;
-  double p, y, d, r, c, s, t, Eik, Eil;
-
+__global__ void INIT1(int N, double *E) {
+  int i, j;
   // Can be parallelized
   for (i = 0; i < N; i++) {
     for (j = 0; j < N; j++) {
       E[INDEX(i, j, N, N)] = (i == j);
     }
   }
+}
 
+__global__ void INIT2(int *state, int N) {
   // Single
-  state = N;
-
+  *state = N;
+}
+__global__ void INIT3(int *ind, double *e, double *S, int N, bool *changed) {
+  int k;
   // Can be easily parallelized
   for (k = 0; k < N - 1; k++) {
-    ind[k] = MAXIND(k);
+    MAXIND(k, N, S, &ind[k]);
     e[k] = S[INDEX(k, k, N, N)];
     changed[k] = true;
   }
+}
 
-  while (state != 0) {
-    m = 0;
-
-    // Parallelize
-    for (k = 1; k < N; k++) {
-      if (fabs(S[INDEX(k, ind[k], N, N)]) > fabs(S[INDEX(m, ind[m], N, N)])) {
-        m = k;
-      }
+__global__ void BEST_M(int *m, int N, double *S, int *ind) {
+  *m = 0;
+  int k;
+  // Parallelize
+  for (k = 1; k < N; k++) {
+    if (fabs(S[INDEX(k, ind[k], N, N)]) > fabs(S[INDEX(*m, ind[*m], N, N)])) {
+      *m = k;
     }
-
-    // Single
-    k = m;
-    l = ind[m];
-    p = S[INDEX(k, l, N, N)];
-    y = (e[l] - e[k]) / 2;
-    d = fabs(y) + sqrt(p * p + y * y);
-    r = sqrt(p * p + d * d);
-    c = d / r;
-    s = p / r;
-    t = p * p / d;
-
-    if (y < 0) {
-      s = -s;
-      t = -t;
-    }
-
-    S[INDEX(k, l, N, N)] = 0.0;
-
-    UPDATE<<1,1>>(k, -t);
-    UPDATE<<1,1>>(l, t);
-
-    // int blockSize = 256;
-    int numBlocks = (k-1 + BLOCKSIZE - 1) / BLOCKSIZE;
-
-    // Can be parallelized
-    ROTATE_MULTIPLE<<numBlocks,BLOCKSIZE>>()
-    // for (i = 0; i < k; i++) {
-      // ROTATE(i, k, i, l, c, s);
-    // }
-
-    // Can be parallelized
-    for (i = k + 1; i < l; i++) {
-      ROTATE(k, i, i, l, c, s);
-    }
-    
-    // Can be parallelized
-    for (i = l + 1; i < N; i++) {
-      ROTATE(k, i, l, i, c, s);
-    }
-
-    // Parallelize Easily
-    for (i = 0; i < N; i++) {
-      Eik = E[INDEX(i, k, N, N)];
-      Eil = E[INDEX(i, l, N, N)];
-      E[INDEX(i, k, N, N)] = c * Eik - s * Eil;
-      E[INDEX(i, l, N, N)] = s * Eik + c * Eil;
-    }
-
-    ind[k] = MAXIND(k);
-    ind[l] = MAXIND(l);
   }
 }
 
-void TRANSPOSE(double *M, int m, int n, double *M_T) {
+__global__ void GET_S_C(int *k, int *l, int m, double *c, double *s, double *t,
+                        int N, int *ind, double *S, double *e) {
+  // Single
+  *k = m;
+  *l = ind[m];
+  double p = S[INDEX(*k, *l, N, N)];
+  double y = (e[*l] - e[*k]) / 2;
+  double d = fabs(y) + sqrt(p * p + y * y);
+  double r = sqrt(p * p + d * d);
+
+  *c = d / r;
+  *s = p / r;
+  *t = p * p / d;
+
+  if (y < 0) {
+    *s = -(*s);
+    *t = -(*t);
+  }
+
+  S[INDEX(*k, *l, N, N)] = 0.0;
+}
+
+__global__ void ROTATE_MULTIPLE1(int k, int l, double c, double s, double *S,
+                                 int N) {
+  int i;
+  // Can be parallelized
+  for (i = 0; i < k; i++) {
+    ROTATE(i, k, i, l, c, s, S, N);
+  }
+}
+
+__global__ void ROTATE_MULTIPLE2(int k, int l, double c, double s, double *S,
+                                 int N) {
+  int i;
+  // Can be parallelized
+  for (i = k + 1; i < l; i++) {
+    ROTATE(k, i, i, l, c, s, S, N);
+  }
+}
+
+__global__ void ROTATE_MULTIPLE3(int k, int l, double c, double s, double *S,
+                                 int N) {
+  int i;
+  // Can be parallelized
+  for (i = l + 1; i < N; i++) {
+    ROTATE(k, i, l, i, c, s, S, N);
+  }
+}
+
+__global__ void UPDATE_E(int N, double *E, int k, int l, double c, double s) {
+  double Eik, Eil;
+  int i;
+  // Parallelize Easily
+  for (i = 0; i < N; i++) {
+    Eik = E[INDEX(i, k, N, N)];
+    Eil = E[INDEX(i, l, N, N)];
+    E[INDEX(i, k, N, N)] = c * Eik - s * Eil;
+    E[INDEX(i, l, N, N)] = s * Eik + c * Eil;
+  }
+}
+
+__global__ void UPDATE_IND(int k, int l, int *ind, int N, double *S) {
+  MAXIND(k, N, S, &ind[k]);
+  MAXIND(l, N, S, &ind[l]);
+}
+
+__global__ void TRANSPOSE(double *M, int m, int n, double *M_T) {
   int i, j;
 
   for (i = 0; i < n; i++) {
@@ -128,7 +144,7 @@ void TRANSPOSE(double *M, int m, int n, double *M_T) {
   }
 }
 
-void MATMUL(double *A, int p, int q, double *B, int r, double *C) {
+__global__ void MATMUL(double *A, int p, int q, double *B, int r, double *C) {
   int i, j, k;
 
   for (i = 0; i < p; i++) {
@@ -141,7 +157,8 @@ void MATMUL(double *A, int p, int q, double *B, int r, double *C) {
   }
 }
 
-void MATMUL_IN_ORDER(double *A, int p, int q, double *B_T, int r, double *C) {
+__global__ void MATMUL_IN_ORDER(double *A, int p, int q, double *B_T, int r,
+                                double *C) {
   int i, j, k;
 
   for (i = 0; i < p; i++) {
@@ -154,14 +171,12 @@ void MATMUL_IN_ORDER(double *A, int p, int q, double *B_T, int r, double *C) {
   }
 }
 
-void SORT(double *e, double *E, int n) {
+__global__ void SORT(double *e, double *E, int n, int *indices, double *new_E) {
 
-  int *indices = (int *)malloc(sizeof(int) * n);
   int temp_int, i, j;
 
   double temp_double;
   double *old_E = E;
-  double *new_E = (double *)malloc(sizeof(double) * n * n);
 
   for (i = 0; i < n; i++) {
     indices[i] = i;
@@ -186,9 +201,121 @@ void SORT(double *e, double *E, int n) {
       new_E[INDEX(j, i, n, n)] = old_E[INDEX(j, indices[i], n, n)];
     }
   }
+}
 
-  free(indices);
-  free(old_E);
+void JACOBI(int n, double *dev_E, double *dev_e, double *dev_S) {
+
+  int *dev_state, *dev_ind, *dev_m, *dev_k, *dev_l;
+  double *dev_c, *dev_s, *dev_t_;
+  bool *dev_changed;
+
+  cudaMalloc(&dev_E, sizeof(double) * n * n);
+  cudaMalloc(&dev_state, sizeof(int));
+  cudaMalloc(&dev_ind, sizeof(int) * n);
+  cudaMalloc(&dev_e, sizeof(double) * n);
+  cudaMalloc(&dev_changed, sizeof(bool) * n);
+  cudaMalloc(&dev_m, sizeof(int));
+  cudaMalloc(&dev_k, sizeof(int));
+  cudaMalloc(&dev_l, sizeof(int));
+  cudaMalloc(&dev_c, sizeof(double));
+  cudaMalloc(&dev_s, sizeof(double));
+  cudaMalloc(&dev_t_, sizeof(double));
+
+  INIT1<<<1, 1>>>(n, dev_E);
+  INIT2<<<1, 1>>>(dev_state, n);
+  INIT3<<<1, 1>>>(dev_ind, dev_e, dev_S, n, dev_changed);
+
+  while (dev_state != 0) {
+
+    BEST_M<<<1, 1>>>(dev_m, n, dev_S, dev_ind);
+
+    GET_S_C<<<1, 1>>>(dev_k, dev_l, *dev_m, dev_c, dev_s, dev_t_, n, dev_ind,
+                      dev_S, dev_e);
+
+    UPDATE<<<1, 1>>>(*dev_k, -(*dev_t_), dev_e, dev_changed, dev_state);
+    UPDATE<<<1, 1>>>(*dev_l, *dev_t_, dev_e, dev_changed, dev_state);
+
+    ROTATE_MULTIPLE1<<<1, 1>>>(*dev_k, *dev_l, *dev_c, *dev_s, dev_S, n);
+    ROTATE_MULTIPLE2<<<1, 1>>>(*dev_k, *dev_l, *dev_c, *dev_s, dev_S, n);
+    ROTATE_MULTIPLE3<<<1, 1>>>(*dev_k, *dev_l, *dev_c, *dev_s, dev_S, n);
+
+    UPDATE_E<<<1, 1>>>(n, dev_E, *dev_k, *dev_l, *dev_c, *dev_s);
+    UPDATE_IND<<<1, 1>>>(*dev_k, *dev_l, dev_ind, n, dev_S);
+  }
+
+  cudaFree(&dev_E);
+  cudaFree(&dev_state);
+  cudaFree(&dev_ind);
+  cudaFree(&dev_e);
+  cudaFree(&dev_changed);
+  cudaFree(&dev_m);
+  cudaFree(&dev_k);
+  cudaFree(&dev_l);
+  cudaFree(&dev_c);
+  cudaFree(&dev_s);
+  cudaFree(&dev_t_);
+}
+
+__global__ void GET_SINGULAR_VALS(int n, double *eigen_total, double *e,
+                                  double *SIGMA, double *SIGMA_INV) {
+  int i;
+  double sqrt_;
+  *eigen_total = 0;
+  /* Square root eigen values to get singular values and Compute SIGMA INVERSE*/
+  for (i = 0; i < n; i++) {
+    *eigen_total += e[i];
+    sqrt_ = sqrt(e[i]);
+    SIGMA[i] = sqrt_;
+    SIGMA_INV[i] = 1 / sqrt_;
+  }
+}
+
+__global__ void GET_U(int m, int n, double *M, double *V_T, double *SIGMA_INV,
+                      double *U) {
+  int i, j, k;
+  double sum;
+  /* Compute M.V.SIGMAINV */
+  for (i = 0; i < m; i++) {
+    for (j = 0; j < m; j++) {
+
+      sum = 0;
+      if (j < n) {
+        for (k = 0; k < n; k++) {
+          sum += M[INDEX(i, k, m, n)] * V_T[INDEX(j, k, n, n)];
+        }
+        sum *= SIGMA_INV[j];
+      }
+
+      U[INDEX(i, j, m, m)] = sum;
+    }
+  }
+}
+
+__global__ void GET_W(int k_retended, int n, double *W, double *E) {
+  int i, j;
+  /* Construct projection matrix */
+  for (i = 0; i < k_retended; i++) {
+    for (j = 0; j < n; j++) {
+      W[INDEX(j, i, n, k_retended)] = E[INDEX(j, i, n, n)];
+    }
+  }
+}
+
+__global__ void GET_RETENTION(int *k, int n, double *e, double eigen_total,
+                              double retention) {
+  int k_retended = 0;
+  double retention_done = 0;
+  /* Choose k largest columns */
+  int i;
+  for (i = 0; i < n; i++) {
+    retention_done += 100 * e[i] / eigen_total;
+    k_retended++;
+    if (retention_done >= retention) {
+      break;
+    }
+  }
+
+  *k = k_retended;
 }
 
 // /*
@@ -199,85 +326,90 @@ void SORT(double *e, double *E, int n) {
 void SVD_and_PCA(int m, int n, double *D, double **U, double **SIGMA,
                  double **V_T, double **D_HAT, int *K, int retention) {
 
-  double *M = D, *M_T, *SIGMA_INV, *MV;
-  double sum, sqrt_;
-  double eigen_total = 0;
-  int i, j, k;
+  double *dev_M, *dev_M_T, *dev_S, *dev_e, *dev_E, *dev_new_E, *dev_eigen_total,
+      *dev_SIGMA, *dev_SIGMA_INV, *dev_V_T, *dev_U, *dev_W, *dev_D_HAT;
 
-  cudaMalloc(&M_T, sizeof(double) * m * n);
-  cudaMalloc(&S, sizeof(double) * n * n);
-  cudaMalloc(&E, sizeof(double) * n * n);
-  cudaMalloc(&e, sizeof(double) * n);
-  cudaMalloc(&SIGMA_INV, sizeof(double) * n);
-  cudaMalloc(&MV, sizeof(double) * m * n);
+  int *dev_k, *dev_indices;
 
-  N = n;
+  cudaMalloc(&dev_M, sizeof(double) * m * n);
+  cudaMalloc(&dev_M_T, sizeof(double) * m * n);
+  cudaMalloc(&dev_S, sizeof(double) * n * n);
 
-  TRANSPOSE(M, m, n, M_T);
+  cudaMemcpy(dev_M, D, sizeof(double) * m * n, cudaMemcpyHostToDevice);
 
-  MATMUL_IN_ORDER(M_T, n, m, M_T, n, S);
+  TRANSPOSE<<<1, 1>>>(dev_M, m, n, dev_M_T);
 
-  JACOBI();
+  MATMUL_IN_ORDER<<<1, 1>>>(dev_M_T, n, m, dev_M_T, n, dev_S);
 
-  SORT(e, E, n);
+  cudaFree(dev_M_T);
 
-  /* Square root eigen values to get singular values and Compute SIGMA INVERSE*/
-  for (i = 0; i < n; i++) {
-    eigen_total += e[i];
-    sqrt_ = sqrt(e[i]);
-    (*SIGMA)[i] = sqrt_;
-    SIGMA_INV[i] = 1 / sqrt_;
-  }
+  cudaMalloc(&dev_e, sizeof(double) * n);
+  cudaMalloc(&dev_E, sizeof(double) * n * n);
+
+  JACOBI(n, dev_E, dev_e, dev_S);
+
+  cudaFree(dev_S);
+
+  cudaMalloc(&dev_indices, sizeof(int) * n);
+  cudaMalloc(&dev_new_E, sizeof(double) * n * n);
+
+  SORT<<<1, 1>>>(dev_e, dev_E, n, dev_indices, dev_new_E);
+
+  cudaFree(dev_indices);
+  cudaFree(dev_E);
+  dev_E = dev_new_E;
+
+  cudaMalloc(&dev_eigen_total, sizeof(int));
+  cudaMalloc(&dev_SIGMA, sizeof(double) * n);
+  cudaMalloc(&dev_SIGMA_INV, sizeof(double) * n);
+  cudaMalloc(&dev_V_T, sizeof(double) * n * n);
+  cudaMalloc(&dev_U, sizeof(double) * m * m);
+
+  GET_SINGULAR_VALS<<<1, 1>>>(n, dev_eigen_total, dev_e, dev_SIGMA,
+                              dev_SIGMA_INV);
 
   /* Compute V_T */
-  TRANSPOSE(E, n, n, *V_T);
+  TRANSPOSE<<<1, 1>>>(dev_E, n, n, dev_V_T);
 
-  /* Compute M.V.SIGMAINV */
-  for (i = 0; i < m; i++) {
-    for (j = 0; j < m; j++) {
+  GET_U<<<1, 1>>>(m, n, dev_M, dev_V_T, dev_SIGMA_INV, dev_U);
 
-      sum = 0;
-      if (j < n) {
-        for (k = 0; k < n; k++) {
-          sum += M[INDEX(i, k, m, n)] * (*V_T)[INDEX(j, k, n, n)];
-        }
-        sum *= SIGMA_INV[j];
-      }
+  cudaFree(dev_SIGMA_INV);
 
-      (*U)[INDEX(i, j, m, m)] = sum;
-    }
-  }
+  cudaMalloc(&dev_k, sizeof(int));
 
-  int k_retended = 0;
-  double retention_done = 0;
-  /* Choose k largest columns */
-  for (i = 0; i < n; i++) {
-    retention_done += 100 * e[i] / eigen_total;
-    k_retended++;
-    if (retention_done >= retention) {
-      break;
-    }
-  }
+  // double *dev_retention;
 
-  double *W = (double *)malloc(sizeof(double) * n * k_retended);
-  /* Construct projection matrix */
-  for (i = 0; i < k_retended; i++) {
-    for (j = 0; j < n; j++) {
-      W[INDEX(j, i, n, k_retended)] = E[INDEX(j, i, n, n)];
-    }
-  }
+  GET_RETENTION<<<1, 1>>>(dev_k, n, dev_e, *dev_eigen_total, retention);
+
+  cudaMemcpy(K, dev_k, sizeof(int), cudaMemcpyDeviceToHost);
+
+  cudaFree(dev_e);
+  cudaFree(dev_eigen_total);
+  cudaFree(dev_k);
+
+  cudaMalloc(&dev_W, sizeof(double) * n * (*K));
+
+  GET_W<<<1, 1>>>(*K, n, dev_W, dev_E);
+
+  cudaFree(dev_E);
+  cudaMalloc(&dev_D_HAT, sizeof(double) * m * (*K));
 
   /* Mat multiply to get d hat */
-  MATMUL(D, m, n, W, k_retended, *D_HAT);
+  MATMUL<<<1, 1>>>(dev_M, m, n, dev_W, *dev_k, dev_D_HAT);
 
-  *K = k;
+  *D_HAT = (double *)malloc(sizeof(double) * m * (*K));
 
-  cudaFree(W);
-  cudaFree(M_T);
-  cudaFree(S);
-  cudaFree(E);
-  cudaFree(e);
-  cudaFree(SIGMA_INV);
-  cudaFree(V_T);
-  cudaFree(MV);
+  cudaMemcpy(*U, dev_U, sizeof(double) * m * m, cudaMemcpyDeviceToHost);
+  cudaMemcpy(*SIGMA, dev_SIGMA, sizeof(double) * n, cudaMemcpyDeviceToHost);
+  cudaMemcpy(*V_T, dev_V_T, sizeof(double) * n * n, cudaMemcpyDeviceToHost);
+  cudaMemcpy(*D_HAT, dev_D_HAT, sizeof(double) * m * (*K),
+             cudaMemcpyDeviceToHost);
+
+  cudaFree(dev_M);
+  cudaFree(dev_W);
+  cudaFree(dev_U);
+  cudaFree(dev_SIGMA);
+  cudaFree(dev_V_T);
+  cudaFree(dev_D_HAT);
+  // *K = k;
 }
