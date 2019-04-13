4c9fdd224ec9ee1cf1f99aa35315eb9f9405e561
Shashwat Shivam <shivamshaswat@gmail.com>
2019-04-13 14:06:07 +0530

    Backup

--- lab3/code/lab3_cuda.cu
+++ lab3/code/lab3_cuda.cu
@@ -1,7 +1,9 @@
 #include "lab3_cuda.h"
 
 #define BLOCKSIZE 32
+#define MAXBLOCKS 65535
 #define JACOBI_TOLERANCE 0.001
+
 __device__ inline int INDEX(int i1, int i2, int l1, int l2) {
   return i1 * l2 + i2;
 }
@@ -18,6 +20,15 @@ __global__ void printMat(double *mat, int n1, int n2) {
 }
 
 __global__ void printVec(double *vec, int n1) {
+  printf("\n");
+  for (int i = 0; i < n1; i++) {
+    printf("%f ", vec[i]);
+  }
+  printf("\n");
+  printf("\n");
+}
+
+__device__ void printVecDev(double *vec, int n1) {
   printf("\n");
   for (int i = 0; i < n1; i++) {
     printf("%f ", vec[i]);
@@ -94,12 +105,6 @@ __global__ void INIT1(int N, double *E) {
   for (i = index; i < N * N; i += stride) {
     E[i] = ((i / N) == (i % N));
   }
-
-  // for (int i = 0; i < N; i++) {
-  //   for (int j = 0; j < N; j++) {
-  //     E[INDEX(i, j, N, N)] = (i == j);
-  //   }
-  // }
 }
 
 __global__ void INIT2(int *state, int N) { *state = N; }
@@ -116,12 +121,6 @@ __global__ void INIT3(int *ind, double *e, double *S, int N, bool *changed) {
     e[k] = S[INDEX(k, k, N, N)];
     changed[k] = true;
   }
-
-  // for (int k = 0; k < N - 1; k++) {
-  //   MAXIND(k, N, S, &ind[k]);
-  //   e[k] = S[INDEX(k, k, N, N)];
-  //   changed[k] = true;
-  // }
 }
 
 // TODO
@@ -166,10 +165,6 @@ __global__ void ROTATE_MULTIPLE1(int *k, int *l, double *c, double *s,
   for (i = index; i < *k; i += stride) {
     ROTATE(i, *k, i, *l, *c, *s, S, N);
   }
-
-  // for (int i = 0; i < *k; i++) {
-  //   ROTATE(i, *k, i, *l, *c, *s, S, N);
-  // }
 }
 
 __global__ void ROTATE_MULTIPLE2(int *k, int *l, double *c, double *s,
@@ -182,10 +177,6 @@ __global__ void ROTATE_MULTIPLE2(int *k, int *l, double *c, double *s,
   for (i = index; i < *l; i += stride) {
     ROTATE(*k, i, i, *l, *c, *s, S, N);
   }
-
-  // for (int i = (*k) + 1; i < *l; i++) {
-  //   ROTATE(*k, i, i, *l, *c, *s, S, N);
-  // }
 }
 
 __global__ void ROTATE_MULTIPLE3(int *k, int *l, double *c, double *s,
@@ -198,10 +189,6 @@ __global__ void ROTATE_MULTIPLE3(int *k, int *l, double *c, double *s,
   for (i = index; i < N; i += stride) {
     ROTATE(*k, i, *l, i, *c, *s, S, N);
   }
-
-  // for (int i = (*l) + 1; i < N; i++) {
-  //   ROTATE(*k, i, *l, i, *c, *s, S, N);
-  // }
 }
 
 __global__ void UPDATE_E(int N, double *E, int *k, int *l, double *c,
@@ -218,13 +205,6 @@ __global__ void UPDATE_E(int N, double *E, int *k, int *l, double *c,
     E[INDEX(i, *k, N, N)] = (*c) * Eik - (*s) * Eil;
     E[INDEX(i, *l, N, N)] = (*s) * Eik + (*c) * Eil;
   }
-
-  // for (int i = 0; i < N; i++) {
-  //   double Eik = E[INDEX(i, *k, N, N)];
-  //   double Eil = E[INDEX(i, *l, N, N)];
-  //   E[INDEX(i, *k, N, N)] = (*c) * Eik - (*s) * Eil;
-  //   E[INDEX(i, *l, N, N)] = (*s) * Eik + (*c) * Eil;
-  // }
 }
 
 __global__ void UPDATE_IND(int *k, int *l, int *ind, int N, double *S) {
@@ -241,12 +221,6 @@ __global__ void TRANSPOSE(double *M, int m, int n, double *M_T) {
   for (i = index; i < n * m; i += stride) {
     M_T[i] = M[INDEX(i % m, i / m, m, n)];
   }
-
-  // for (int i = 0; i < n; i++) {
-  //   for (int j = 0; j < m; j++) {
-  //     M_T[INDEX(i, j, n, m)] = M[INDEX(j, i, m, n)];
-  //   }
-  // }
 }
 
 // TODO
@@ -263,110 +237,22 @@ __global__ void MATMUL(double *A, int p, int q, double *B, int r, double *C) {
   }
 }
 
-__device__ double *GET_SUB_MATRIX_START_POS(double *mat, int w, int row,
-                                            int col) {
-  return &mat[w * BLOCKSIZE * row + BLOCKSIZE * col];
-}
-
-// TO TEST
-__global__ void MATMUL_OPTIMIZED(int n1, int n2, int n3, double *A, double *B,
-                                 double *C) {
-
-  int blockRow = blockIdx.x;
-  int blockCol = blockIdx.y;
-  int row = threadIdx.x;
-  int col = threadIdx.y;
-  double Cvalue = 0;
-
-  double *Csub = GET_SUB_MATRIX_START_POS(C, n3, blockRow, blockCol);
-  // Matrix Csub = GetSubMatrix(C, blockRow, blockCol);
-
-  for (int m = 0; m < (n2 / BLOCKSIZE); m++) {
-    double *Asub = GET_SUB_MATRIX_START_POS(A, n2, blockRow, m);
-    // Matrix Asub = GetSubMatrix(A, blockRow, m);
-    double *Bsub = GET_SUB_MATRIX_START_POS(B, n3, m, blockCol);
-    // Matrix Bsub = GetSubMatrix(B, m, blockCol);
-
-    __shared__ double As[BLOCKSIZE][BLOCKSIZE];
-    __shared__ double Bs[BLOCKSIZE][BLOCKSIZE];
-
-    As[row][col] = Asub[row * n2 + col];
-    // As[row][col] = GetElement(Asub, row, col);
-    Bs[row][col] = Bsub[row * n3 + col];
-    // Bs[row][col] = GetElement(Bsub, row, col);
-
-    __syncthreads();
-
-    for (int e = 0; e < BLOCKSIZE; e++) {
-      Cvalue += As[row][e] * Bs[e][col];
-    }
-
-    __syncthreads();
-  }
-
-  Csub[row * n3 + col] = Cvalue;
-  // SetElement(Csub, row, col, Cvalue);
-}
-
-// TODO
-__global__ void MATMUL_IN_ORDER(double *A, int p, int q, double *B_T, int r,
-                                double *C) {
-  int i, j, k;
-
-  for (i = 0; i < p; i++) {
-    for (j = 0; j < r; j++) {
-      C[INDEX(i, j, p, r)] = 0;
-      for (k = 0; k < q; k++) {
-        C[INDEX(i, j, p, r)] += A[INDEX(i, k, p, q)] * B_T[INDEX(j, k, r, q)];
-      }
-    }
-  }
-}
-
-// TODO
-__global__ void SORT(double *e, double *E, int n, int *indices, double *new_E) {
-
-  int temp_int, i, j;
-
-  double temp_double;
-  double *old_E = E;
-
-  for (i = 0; i < n; i++) {
-    indices[i] = i;
-  }
-
-  for (i = 0; i < n; i++) {
-    for (j = 0; j < n; j++) {
-      if (e[i] > e[j]) {
-        temp_double = e[i];
-        e[i] = e[j];
-        e[j] = temp_double;
-
-        temp_int = indices[i];
-        indices[i] = indices[j];
-        indices[j] = temp_int;
-      }
-    }
-  }
-
-  for (i = 0; i < n; i++) {
-    for (j = 0; j < n; j++) {
-      new_E[INDEX(j, i, n, n)] = old_E[INDEX(j, indices[i], n, n)];
-    }
-  }
-}
-
-__global__ void ODD_EVEN_SORT(double *arr, int n, bool *converged) {
+__global__ void ODD_EVEN_SORT(double *arr, int *indices, int n,
+                              bool *converged) {
 
   int index_global = blockIdx.x * blockDim.x + threadIdx.x;
   int stride = blockDim.x * gridDim.x;
 
-  *converged = true;
+  *converged = false;
   bool odd_iter = false;
   double temp;
-  int to_see, to_see_next, index_local;
+  int to_see, to_see_next, index_local, i, temp_int;
 
-  while (!*converged) {
+  for (i = index_global; i < n; i += stride) {
+    indices[i] = i;
+  }
+
+  while (!(*converged)) {
     *converged = true;
     for (index_local = index_global; index_local < n / 2;
          index_local += stride) {
@@ -374,24 +260,48 @@ __global__ void ODD_EVEN_SORT(double *arr, int n, bool *converged) {
         to_see = 2 * index_local + 1;
         to_see_next = 2 * index_local + 2;
         if (arr[to_see] < arr[to_see_next]) {
+
           temp = arr[to_see_next];
           arr[to_see_next] = arr[to_see];
-          arr[to_see_next] = temp;
+          arr[to_see] = temp;
+
+          temp_int = indices[to_see_next];
+          indices[to_see_next] = indices[to_see];
+          indices[to_see] = temp_int;
+
           *converged = false;
         }
-      } else if (2 * index_local + 1 < n) {
+      } else if (!odd_iter && 2 * index_local + 1 < n) {
         to_see = 2 * index_local;
         to_see_next = 2 * index_local + 1;
         if (arr[to_see] < arr[to_see_next]) {
+
           temp = arr[to_see_next];
           arr[to_see_next] = arr[to_see];
-          arr[to_see_next] = temp;
+          arr[to_see] = temp;
+
+          temp_int = indices[to_see_next];
+          indices[to_see_next] = indices[to_see];
+          indices[to_see] = temp_int;
+
           *converged = false;
         }
       }
     }
+
     odd_iter = !odd_iter;
     __syncthreads();
+  }
+}
+
+__global__ void ARRANGE(int *indices, double *old_E, double *new_E, int n1,
+                        int n2) {
+
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+
+  for (int i = index; i < n1 * n2; i += stride) {
+    new_E[i] = old_E[INDEX(i / n2, indices[i % n2], n1, n2)];
   }
 }
 
@@ -412,24 +322,16 @@ void JACOBI(int n, double *dev_E, double *dev_e, double *dev_S) {
   cudaMalloc(&dev_s, sizeof(double));
   cudaMalloc(&dev_t_, sizeof(double));
 
-  // printf("Starting INIT\n");
   int numblocks = (min(n, 65536) + BLOCKSIZE - 1) / BLOCKSIZE;
 
   INIT1<<<numblocks, BLOCKSIZE>>>(n, dev_E);
   INIT2<<<1, 1>>>(dev_state, n);
   INIT3<<<numblocks, BLOCKSIZE>>>(dev_ind, dev_e, dev_S, n, dev_changed);
-  // printVec<<<1, 1>>>(dev_e, n);
-  // printVec<<<1, 1>>>(dev_ind, n);
-  // printVec<<<1, 1>>>(dev_changed, n);
-  // printMat<<<1, 1>>>(dev_E, n, n);
-
-  // printf("Starting WHILE loop\n");
 
   int count = 0;
 
   while (state != 0 && count < 100) {
     count++;
-    // printf("state = %d, count = %d\n", state, count);
 
     BEST_M<<<1, 1>>>(dev_m, n, dev_S, dev_ind);
     GET_S_C<<<1, 1>>>(dev_k, dev_l, dev_m, dev_c, dev_s, dev_t_, n, dev_ind,
@@ -449,14 +351,7 @@ void JACOBI(int n, double *dev_E, double *dev_e, double *dev_S) {
     cudaMemcpy(&state, dev_state, sizeof(int), cudaMemcpyDeviceToHost);
 
     cudaDeviceSynchronize();
-
-    // printMat<<<1, 1>>>(dev_S, n, n);
   }
-
-  // printf("JACOBI Converged\n");
-
-  // printMat<<<1, 1>>>(dev_E, n, n);
-  // printVec<<<1, 1>>>(dev_e, n);
 
   cudaFree(dev_state);
   cudaFree(dev_ind);
@@ -481,12 +376,6 @@ __global__ void GET_SINGULAR_VALS(int n, double *e, double *SIGMA,
     SIGMA[i] = sqrt_;
     SIGMA_INV[i] = 1 / sqrt_;
   }
-
-  // for (int i = 0; i < n; i++) {
-  //   double sqrt_ = sqrt(e[i]);
-  //   SIGMA[i] = sqrt_;
-  //   SIGMA_INV[i] = 1 / sqrt_;
-  // }
 }
 
 // TODO
@@ -495,6 +384,19 @@ __global__ void GET_EIGEN_SUM(double *eigen_total, double *e, int n) {
   *eigen_total = 0;
   for (i = 0; i < n; i++) {
     *eigen_total += e[i];
+  }
+}
+
+__global__ void MULTIPLY_SIGMA_INV(double *MV, double *SIGMA_INV, double *U,
+                                   int m, int n) {
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  int i;
+
+  for (i = index; i < m * m; i += stride) {
+    if (i / m < n) {
+      U[i] = MV[i % m] * SIGMA_INV[i % m];
+    }
   }
 }
 
@@ -508,37 +410,13 @@ void GET_U(int m, int n, double *dev_M, double *dev_V_T, double *dev_SIGMA_INV,
 
   TRANSPOSE<<<numblocks, BLOCKSIZE>>>(dev_V_T, n, n, dev_V);
   numblocks = (min(m * n, 65536) + BLOCKSIZE - 1) / BLOCKSIZE;
-  MATMUL_OPTIMIZED<<<numblocks, BLOCKSIZE>>>(m, n, n, dev_M, dev_V, dev_MV);
+  // MATMUL_OPTIMIZED<<<numblocks, BLOCKSIZE>>>(m, n, n, dev_M, dev_V, dev_MV);
   cudaFree(dev_MV);
   cudaFree(dev_V);
 
   // numblocks = (min(m * n, 65536) + BLOCKSIZE - 1) / BLOCKSIZE;
   // SIGMA_MULTI<<<
-
 }
-
-// // TODO
-// __global__ void GET_U(int m, int n, double *M, double *V_T, double
-// *SIGMA_INV,
-//                       double *U) {
-//   int i, j, k;
-//   double sum;
-//   /* Compute M.V.SIGMAINV */
-//   for (i = 0; i < m; i++) {
-//     for (j = 0; j < m; j++) {
-
-//       sum = 0;
-//       if (j < n) {
-//         for (k = 0; k < n; k++) {
-//           sum += M[INDEX(i, k, m, n)] * V_T[INDEX(j, k, n, n)];
-//         }
-//         sum *= SIGMA_INV[j];
-//       }
-
-//       U[INDEX(i, j, m, m)] = sum;
-//     }
-//   }
-// }
 
 __global__ void GET_W(int k_retended, int n, double *W, double *E) {
 
@@ -549,20 +427,14 @@ __global__ void GET_W(int k_retended, int n, double *W, double *E) {
   for (i = index; i < n * k_retended; i += stride) {
     W[i] = E[INDEX(i / k_retended, i % k_retended, n, n)];
   }
-
-  // for (int i = 0; i < k_retended; i++) {
-  //   for (int j = 0; j < n; j++) {
-  //     W[INDEX(j, i, n, k_retended)] = E[INDEX(j, i, n, n)];
-  //   }
-  // }
 }
 
 __global__ void GET_RETENTION(int *k, int n, double *e, double *eigen_total,
                               double retention) {
   int k_retended = 0;
   double retention_done = 0;
-  /* Choose k largest columns */
   int i;
+
   for (i = 0; i < n; i++) {
     retention_done += 100 * e[i] / *eigen_total;
     k_retended++;
@@ -574,79 +446,64 @@ __global__ void GET_RETENTION(int *k, int n, double *e, double *eigen_total,
   *k = k_retended;
 }
 
-// /*
-// 	*****************************************************
-// 		TODO -- You must implement this function
-// 	*****************************************************
-// */
 void SVD_and_PCA(int m, int n, double *D, double **U, double **SIGMA,
                  double **V_T, double **D_HAT, int *K, int retention) {
-
-  // printf("Starting SVD\n");
 
   double *dev_M, *dev_M_T, *dev_S, *dev_e, *dev_E, *dev_new_E, *dev_eigen_total,
       *dev_SIGMA, *dev_SIGMA_INV, *dev_V_T, *dev_U, *dev_W, *dev_D_HAT;
 
-  int *dev_k, *dev_indices;
-
-
-
-
-  // printf("Memory Allocated\n");
-  int numblocks = (min(m * n, 65535) + BLOCKSIZE - 1) / BLOCKSIZE;
+  int *dev_k, *dev_indices,
+      numblocks = (min(m * n, MAXBLOCKS) + BLOCKSIZE - 1) / BLOCKSIZE;
 
   cudaMalloc(&dev_M, sizeof(double) * m * n);
   cudaMemcpy(dev_M, D, sizeof(double) * m * n, cudaMemcpyHostToDevice);
   cudaMalloc(&dev_M_T, sizeof(double) * m * n);
+
   TRANSPOSE<<<numblocks, BLOCKSIZE>>>(dev_M, m, n, dev_M_T);
 
-  // printf("dev_M_T calculated\n");
-
-  // MATMUL_IN_ORDER<<<1, 1>>>(dev_M_T, n, m, dev_M_T, n, dev_S);
   cudaMalloc(&dev_S, sizeof(double) * n * n);
-  MATMUL_OPTIMIZED<<<numblocks, BLOCKSIZE>>>(n, m, n, dev_M_T, dev_M, dev_S);
+  // MATMUL_IN_ORDER<<<1, 1>>>(dev_M_T, n, m, dev_M_T, n, dev_S);
+
   cudaFree(dev_M_T);
 
-  // printMat<<<1, 1>>>(dev_S, n, n);
-
-  // printf("MATMUL done. Starting JACOBI\n");
   cudaMalloc(&dev_e, sizeof(double) * n);
   cudaMalloc(&dev_E, sizeof(double) * n * n);
   JACOBI(n, dev_E, dev_e, dev_S);
   cudaFree(dev_S);
 
-  // printf("Starting SORT\n");
   cudaMalloc(&dev_indices, sizeof(int) * n);
   cudaMalloc(&dev_new_E, sizeof(double) * n * n);
-  SORT<<<1, 1>>>(dev_e, dev_E, n, dev_indices, dev_new_E);
+
+  bool *converged;
+  cudaMalloc(&converged, sizeof(bool));
+  numblocks = (min(n, MAXBLOCKS) + BLOCKSIZE - 1) / BLOCKSIZE;
+  ODD_EVEN_SORT<<<numblocks, BLOCKSIZE>>>(dev_e, dev_indices, n, converged);
+  cudaFree(converged);
+
+  numblocks = (min(n * n, MAXBLOCKS) + BLOCKSIZE - 1) / BLOCKSIZE;
+  ARRANGE<<<numblocks, BLOCKSIZE>>>(dev_indices, dev_E, dev_new_E, n, n);
   cudaFree(dev_indices);
 
-  // ODD_EVEN_SORT<<<1, 1>>>(dev_e, dev_E, n, dev_indices, dev_new_E);
-  // printVec<<<1, 1>>>(dev_e, n);
-  double *dev_old_E = dev_E;
-  cudaFree(dev_old_E);
+  cudaFree(dev_E);
   dev_E = dev_new_E;
-  // printMat<<<1, 1>>>(dev_E, n, n);
 
-  // printf("Calculating SINGULAR Values\n");
   cudaMalloc(&dev_SIGMA, sizeof(double) * n);
   cudaMalloc(&dev_SIGMA_INV, sizeof(double) * n);
-  GET_SINGULAR_VALS<<<1, 1>>>(n, dev_e, dev_SIGMA, dev_SIGMA_INV);
+  numblocks = (min(n, MAXBLOCKS) + BLOCKSIZE - 1) / BLOCKSIZE;
+  GET_SINGULAR_VALS<<<numblocks, BLOCKSIZE>>>(n, dev_e, dev_SIGMA,
+                                              dev_SIGMA_INV);
+
   cudaMalloc(&dev_eigen_total, sizeof(int));
   GET_EIGEN_SUM<<<1, 1>>>(dev_eigen_total, dev_e, n);
 
-  // printf("Calculating V_T\n");
   cudaMalloc(&dev_V_T, sizeof(double) * n * n);
+  numblocks = (min(n * n, MAXBLOCKS) + BLOCKSIZE - 1) / BLOCKSIZE;
   TRANSPOSE<<<numblocks, BLOCKSIZE>>>(dev_E, n, n, dev_V_T);
 
-  // printf("Calculating U\n");
-
-  // GET_U<<<1, 1>>>(m, n, dev_M, dev_V_T, dev_SIGMA_INV, dev_U);
   cudaMalloc(&dev_U, sizeof(double) * m * m);
   GET_U(m, n, dev_M, dev_V_T, dev_SIGMA_INV, dev_U);
   cudaFree(dev_SIGMA_INV);
 
-  // printf("Calculating retention k\n");
   cudaMalloc(&dev_k, sizeof(int));
   GET_RETENTION<<<1, 1>>>(dev_k, n, dev_e, dev_eigen_total, retention);
   cudaFree(dev_eigen_total);
@@ -659,24 +516,14 @@ void SVD_and_PCA(int m, int n, double *D, double **U, double **SIGMA,
   cudaMalloc(&dev_D_HAT, sizeof(double) * m * (*K));
   *D_HAT = (double *)malloc(sizeof(double) * m * (*K));
 
-  // printf("Calculating W\n");
-
   GET_W<<<1, 1>>>(*K, n, dev_W, dev_E);
   cudaFree(dev_E);
 
-  // printf("Calculating D_HAT\n");
-
   numblocks = (min(n, 65535) + BLOCKSIZE - 1) / BLOCKSIZE;
-  // MATMUL<<<1, 1>>>(dev_M, m, n, dev_W, *K, dev_D_HAT);
-  MATMUL_OPTIMIZED<<<numblocks, BLOCKSIZE>>>(m, n, *K, dev_M, dev_W, dev_D_HAT);
+  MATMUL<<<1, 1>>>(dev_M, m, n, dev_W, *K, dev_D_HAT);
+
   cudaFree(dev_W);
   cudaFree(dev_M);
-
-  // printf("Calculated D_HAT %d\n", sizeof(double) * m * m);
-
-
-  // printMat<<<1, 1>>>(dev_V_T, n, n);
-  // printMat<<<1, 1>>>(dev_U, m, m);
 
   cudaMemcpy(*V_T, dev_U, sizeof(double) * m * m, cudaMemcpyDeviceToHost);
   cudaFree(dev_U);
@@ -692,8 +539,4 @@ void SVD_and_PCA(int m, int n, double *D, double **U, double **SIGMA,
   cudaFree(dev_D_HAT);
 
   cudaDeviceSynchronize();
-
-  // printf("Cleaned up\n");
-
-  // *K = k;
 }
---
+++ lab3/code/unused.cu
@@ -0,0 +1,132 @@
+
+// TODO
+__global__ void MATMUL_IN_ORDER(double *A, int p, int q, double *B_T, int r,
+                                double *C) {
+  int i, j, k;
+
+  for (i = 0; i < p; i++) {
+    for (j = 0; j < r; j++) {
+      C[INDEX(i, j, p, r)] = 0;
+      for (k = 0; k < q; k++) {
+        C[INDEX(i, j, p, r)] += A[INDEX(i, k, p, q)] * B_T[INDEX(j, k, r, q)];
+      }
+    }
+  }
+}
+
+__device__ double *GET_SUB_MATRIX_START_POS(double *mat, int w, int row,
+                                            int col) {
+  return &mat[w * BLOCKSIZE * row + BLOCKSIZE * col];
+}
+
+// TO TEST
+__global__ void MATMUL_OPTIMIZED(int n1, int n2, int n3, double *A, double *B,
+                                 double *C) {
+
+  int blockRow = blockIdx.x;
+  int blockCol = blockIdx.y;
+  int row = threadIdx.x;
+  int col = threadIdx.y;
+  double Cvalue = 0;
+
+  double *Csub = GET_SUB_MATRIX_START_POS(C, n3, blockRow, blockCol);
+  // Matrix Csub = GetSubMatrix(C, blockRow, blockCol);
+  // printf("%d %d\n", n2, (n2 + BLOCKSIZE - 1) / BLOCKSIZE);
+
+  for (int block_num = 0; block_num < ((n2 + BLOCKSIZE - 1) / BLOCKSIZE);
+       block_num++) {
+    double *Asub = GET_SUB_MATRIX_START_POS(A, n2, blockRow, block_num);
+    // Matrix Asub = GetSubMatrix(A, blockRow, m);
+    double *Bsub = GET_SUB_MATRIX_START_POS(B, n3, block_num, blockCol);
+    // Matrix Bsub = GetSubMatrix(B, m, blockCol);
+
+    __shared__ double As[BLOCKSIZE][BLOCKSIZE];
+    __shared__ double Bs[BLOCKSIZE][BLOCKSIZE];
+
+    As[row][col] = Asub[row * n2 + col];
+    // As[row][col] = GetElement(Asub, row, col);
+    Bs[row][col] = Bsub[row * n3 + col];
+    // Bs[row][col] = GetElement(Bsub, row, col);
+
+    __syncthreads();
+
+    for (int e = 0; e < BLOCKSIZE; e++) {
+      Cvalue += As[row][e] * Bs[e][col];
+    }
+
+    __syncthreads();
+  }
+
+  Csub[row * n3 + col] = Cvalue;
+  // SetElement(Csub, row, col, Cvalue);
+}
+
+// TODO
+__global__ void SORT(double *e, double *E, int n, int *indices, double *new_E) {
+
+  int temp_int, i, j;
+
+  double temp_double;
+  double *old_E = E;
+
+  for (i = 0; i < n; i++) {
+    indices[i] = i;
+  }
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      if (e[i] > e[j]) {
+        temp_double = e[i];
+        e[i] = e[j];
+        e[j] = temp_double;
+
+        temp_int = indices[i];
+        indices[i] = indices[j];
+        indices[j] = temp_int;
+      }
+    }
+  }
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      new_E[INDEX(j, i, n, n)] = old_E[INDEX(j, indices[i], n, n)];
+    }
+  }
+}
+
+
+// // TODO
+// __global__ void GET_U(int m, int n, double *M, double *V_T, double
+// *SIGMA_INV,
+//                       double *U) {
+//   int i, j, k;
+//   double sum;
+//   /* Compute M.V.SIGMAINV */
+//   for (i = 0; i < m; i++) {
+//     for (j = 0; j < m; j++) {
+
+//       sum = 0;
+//       if (j < n) {
+//         for (k = 0; k < n; k++) {
+//           sum += M[INDEX(i, k, m, n)] * V_T[INDEX(j, k, n, n)];
+//         }
+//         sum *= SIGMA_INV[j];
+//       }
+
+//       U[INDEX(i, j, m, m)] = sum;
+//     }
+//   }
+// }
+
+
+  // dim3 threadsPerBlock(BLOCKSIZE, BLOCKSIZE);
+  // dim3 blocksPerGrid(n / threadsPerBlock.x, n / threadsPerBlock.y);
+  // MATMUL_OPTIMIZED<<<blocksPerGrid, threadsPerBlock>>>(n, m, n, dev_M_T,
+  // dev_M,
+  // dev_S);
+  //      
+
+  // GET_U<<<1, 1>>>(m, n, dev_M, dev_V_T, dev_SIGMA_INV, dev_U);
+  // MATMUL_OPTIMIZED<<<numblocks,
+  // BLOCKSIZE>>>(m, n, *K, dev_M,
+  // dev_W, dev_D_HAT);
